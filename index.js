dfb5580bc5df29c169f0f907ef1f57ced83bb7df
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-10-22 13:48:46 +0100

    commit message

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ function getEmotionsArray(cats){
         
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-
-            emotionsArray.push(emotion)
+            if(!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
         }
     }
     return emotionsArray
