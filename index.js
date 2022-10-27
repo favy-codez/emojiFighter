de38be47c9f9fe88884dc2f137e141aac8211e41
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-10-27 12:06:53 +0100

    worked on the js file

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ function highlightCheckedOption(e){
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
 
+// we want to render this fxn when the gif checkbox is clicked 
+function getMatchingCatsArray(){
+    const isGif = gifOption.checked
+}
 
 function getEmotionsArray(cats){
     const emotionsArray = []
@@ -65,10 +69,3 @@ function renderEmotionsRadios(cats){
 }
 
 renderEmotionsRadios(catsData)
-
-
-// we want to render this fxn when the gif checkbox is clicked 
-function getMatchingCatsArray(){
-    const isGif = gifOption.checked
-}
-
